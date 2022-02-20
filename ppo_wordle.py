6a99494366e6d7e7cb8ac74bc2222cdd83aebc7e
conradry <conradry@bc.edu>
2022-02-19 20:03:52 -0500

    initial commit

---
+++ ppo_wordle.py
@@ -0,0 +1,211 @@
+import gym
+import math
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Categorical
+import scipy.signal
+from wordle import WordleEnv
+
+def discount_cumsum(x, discount):
+    return scipy.signal.lfilter([1], [1, float(-discount)], x[::-1], axis=0)[::-1]
+
+class PPOBuffer:
+    def __init__(self, obs_dim, size, gamma=0.99, lamb=0.95):
+        self.obs_buf = np.zeros((size, obs_dim), dtype=np.float32)
+        self.act_buf = np.zeros(size, dtype=np.float32)
+        self.adv_buf = np.zeros(size, dtype=np.float32)
+        self.rew_buf = np.zeros(size, dtype=np.float32)
+        self.ret_buf = np.zeros(size, dtype=np.float32)
+        self.val_buf = np.zeros(size, dtype=np.float32)
+        self.logp_buf = np.zeros(size, dtype=np.float32)
+        self.gamma, self.lam = gamma, lamb
+        self.ptr, self.path_start_idx, self.max_size = 0, 0, size
+
+    def store(self, obs, act, rew, val, logp):
+        assert self.ptr < self.max_size     # buffer has to have room so you can store
+        self.obs_buf[self.ptr] = obs
+        self.act_buf[self.ptr] = act
+        self.rew_buf[self.ptr] = rew
+        self.val_buf[self.ptr] = val
+        self.logp_buf[self.ptr] = logp
+        self.ptr += 1
+
+    def finish_path(self, last_val=0):
+        path_slice = slice(self.path_start_idx, self.ptr)
+        rews = np.append(self.rew_buf[path_slice], last_val)
+        vals = np.append(self.val_buf[path_slice], last_val)
+        
+        # the next two lines implement GAE-Lambda advantage calculation
+        deltas = rews[:-1] + self.gamma * vals[1:] - vals[:-1]
+        self.adv_buf[path_slice] = discount_cumsum(deltas, self.gamma * self.lam)
+        
+        # the next line computes rewards-to-go, to be targets for the value function
+        self.ret_buf[path_slice] = discount_cumsum(rews, self.gamma)[:-1]
+        
+        self.path_start_idx = self.ptr
+
+    def get(self):
+        assert self.ptr == self.max_size    # buffer has to be full before you can get
+        self.ptr, self.path_start_idx = 0, 0
+        # the next two lines implement the advantage normalization trick
+        adv_mean, adv_std = self.adv_buf.mean(), self.adv_buf.std()
+        self.adv_buf = (self.adv_buf - adv_mean) / adv_std
+        data = dict(obs=self.obs_buf, act=self.act_buf, ret=self.ret_buf,
+                    adv=self.adv_buf, logp=self.logp_buf)
+        return {k: torch.as_tensor(v, dtype=torch.float32) for k,v in data.items()}
+
+class MLP(nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, action_dim)
+        self.relu = nn.ReLU(inplace=True)
+
+    def _distribution(self, state):
+        logits = self.run_net(state)
+        return Categorical(logits=logits)
+
+    def _log_prob_from_distribution(self, dist, action):
+        return dist.log_prob(action)
+
+    def run_net(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        return self.fc3(x)
+
+    def forward(self, obs, act=None):
+        pi = self._distribution(obs)
+        logp_a = None
+        if act is not None:
+            logp_a = self._log_prob_from_distribution(pi, act)
+
+        return pi, logp_a
+
+class MLPActorCritic(nn.Module):
+    def __init__(self, obs_dim, hidden_dim, act_dim, emb_dim):
+        super().__init__()
+        self.pi = MLP(obs_dim, hidden_dim, act_dim)
+        self.v  = MLP(obs_dim, hidden_dim, 1)
+        self.w_embed = nn.Embedding(act_dim, emb_dim)
+        self.s_embed = nn.Embedding(4, 3)
+
+    def embed_obs(self, obs):
+        words = obs[:, :6]
+        scores = obs[:, 6:]
+        words = self.w_embed(words).flatten(1)
+        scores = self.s_embed(scores).flatten(1)
+        obs_emb = torch.cat([words, scores], dim=1)
+        #print(words.shape, scores.shape, obs_emb.shape)
+        return obs_emb 
+
+    def step(self, obs):
+        obs = self.embed_obs(obs)
+        with torch.no_grad():
+            pi = self.pi._distribution(obs)
+            a = pi.sample()
+            logp_a = self.pi._log_prob_from_distribution(pi, a)
+            v = self.v.run_net(obs)
+        return a.numpy(), v.numpy(), logp_a.numpy()
+
+    def act(self, obs):
+        return self.step(obs)[0]
+
+if __name__ == '__main__':
+
+    env = WordleEnv()
+    obs_dim = 6 + 6 * 5
+    act_dim = env.n_actions
+    hidden_dim = 32
+    emb_dim = 32
+
+    obs_emb_dim = 6 * emb_dim + 6 * 5 * 3
+
+    gamma = 0.99
+    lamb = 0.95
+    steps = 3000
+    epochs = 50
+    clip_ratio = 0.2
+    pi_lr = 3e-4
+    vf_lr = 1e-3
+    target_kl = 0.01
+
+    ac = MLPActorCritic(obs_emb_dim, hidden_dim, act_dim, emb_dim)
+    buf = PPOBuffer(obs_dim, size=steps, gamma=gamma, lamb=lamb)
+    
+    # Set up optimizers for policy and value function
+    pi_optimizer = torch.optim.Adam(ac.pi.parameters(), lr=pi_lr)
+    vf_optimizer = torch.optim.Adam(ac.v.parameters(), lr=vf_lr)
+
+    o, ep_ret, ep_len = env.reset(), 0, 0
+
+    # Main loop: collect experience in env and update/log each epoch
+    for epoch in range(epochs):
+        print('Running epoch', epoch)
+        ep_ret = 0
+        for t in range(steps):
+            if epoch >= epochs - 1:
+                env.render()
+            a, v, logp = ac.step(torch.as_tensor(o[None], dtype=torch.long))
+
+            next_o, r, d, _ = env.step(a[0])
+            ep_ret += r
+            ep_len += 1
+
+            # save and log
+            buf.store(o, a, r, v, logp)
+            
+            # Update obs (critical!)
+            o = next_o
+
+            terminal = d
+            epoch_ended = t == steps - 1
+
+            if terminal or epoch_ended:
+                # if trajectory didn't reach terminal state, bootstrap value target
+                if epoch_ended:
+                    _, v, _ = ac.step(torch.as_tensor(o[None], dtype=torch.long))
+                    print('Total reward', ep_ret)
+                else:
+                    v = 0
+
+                buf.finish_path(v)
+                o, ep_len = env.reset(), 0
+
+        data = buf.get()
+        obs, act, adv, logp_old = data['obs'], data['act'], data['adv'], data['logp']
+        obs = obs.long()
+        ret = data['ret']
+
+        # Train policy with multiple steps of gradient descent
+        for i in range(80):
+            pi_optimizer.zero_grad()
+
+            # Policy loss
+            pi, logp = ac.pi(ac.embed_obs(obs), act)
+            ratio = torch.exp(logp - logp_old)
+            clip_adv = torch.clamp(ratio, 1-clip_ratio, 1+clip_ratio) * adv
+            loss_pi = -(torch.min(ratio * adv, clip_adv)).mean()
+
+            # Useful extra info
+            approx_kl = (logp_old - logp).mean().item()
+            ent = pi.entropy().mean().item()
+            if approx_kl > 1.5 * target_kl:
+                break
+
+            loss_pi.backward()
+            pi_optimizer.step()
+
+        # Value function learning
+        for i in range(80):
+            vf_optimizer.zero_grad()
+            loss_v = ((ac.v.run_net(ac.embed_obs(obs)) - ret)**2).mean()
+            loss_v.backward()
+            vf_optimizer.step()
+
+        # Log changes from update
+        print('Entropy', ent)
+        print('Policy loss', loss_pi.item())
+        print('Value loss', loss_v.item())
---
+++ wordle.py
@@ -0,0 +1,154 @@
+import pyglet
+import time
+import math
+import random
+from typing import Optional
+
+import numpy as np
+from collections import Counter
+from words import word_list
+
+def get_display(spec):
+    """Convert a display specification (such as :0) into an actual Display
+    object.
+    Pyglet only supports multiple Displays on Linux.
+    """
+    if spec is None:
+        return pyglet.canvas.get_display()
+        # returns already available pyglet_display,
+        # if there is no pyglet display available then it creates one
+    elif isinstance(spec, str):
+        return pyglet.canvas.Display(spec)
+    else:
+        raise error.Error(
+            f"Invalid display specification: {spec}. (Must be a string like :0 or None.)"
+        )
+
+
+def get_window(width, height, display, **kwargs):
+    """
+    Will create a pyglet window from the display specification provided.
+    """
+    screen = display.get_screens()  # available screens
+    config = screen[0].get_best_config()  # selecting the first screen
+    context = config.create_context(None)  # create GL context
+
+    return pyglet.window.Window(
+        width=width,
+        height=height,
+        display=display,
+        config=config,
+        context=context,
+        **kwargs,
+    )
+
+def make_observation(target, guess):
+    letter_counts = dict(Counter(target))
+
+    observation = [1] * 5 # all null
+    guess_letters_used = {c: 0 for c in guess}
+
+    # fill in the exact hits
+    for i, (cg,tg) in enumerate(zip(guess, target)):
+        if cg == tg:
+            observation[i] = 3 
+            guess_letters_used[cg] += 1
+
+    # fill in the wrong position hits
+    for i, (cg,tg) in enumerate(zip(guess, target)):
+        if cg in letter_counts and observation[i] == 0:
+            if guess_letters_used[cg] < letter_counts[cg]:
+                observation[i] = 2
+                guess_letters_used[cg] += 1
+
+    return observation
+
+class WordleEnv:
+    def __init__(self):
+        self.viewer = None
+        self.n_actions = len(word_list)
+        self.guessed_words = []
+        self.letter_colors = []
+        self.color_lookup = {
+            0: (0, 0, 0, 0), 
+            1: (255, 0, 0, 255), 
+            2: (0, 0, 255, 255),
+            3: (0, 255, 0, 255),
+        }
+        display = get_display(None)
+        self.screen_width = 64
+        self.screen_height = 96
+        self.window = get_window(self.screen_width, self.screen_height, display)
+        
+        self.actions = 6 * [0] # 6 words
+        self.scores = 6  * [5 * [0]] # 6 words * 5 letters
+
+    def step(self, action):
+        # get the word from the action
+        guess = word_list[action]
+        score = make_observation(self.secret_word, guess)
+        self.guessed_words.append(guess)
+        self.letter_colors.append([self.color_lookup[i] for i in score])
+        
+        reward = 1 if all([s == 3 for s in score]) else 0 
+        
+        # update the state
+        done = False
+        if len(self.guessed_words) == 6 or reward == 1:
+            done = True
+        
+        #state = self.render(return_image=True)        
+        n_guesses = len(self.guessed_words)
+        self.actions[n_guesses - 1] = action
+        self.scores[n_guesses - 1] = score
+        state = np.concatenate([self.actions, *self.scores], axis=0) 
+
+        return state, reward, done, {}
+
+    def reset(self, seed: Optional[int] = None):
+        # set a random word
+        self.window.clear()
+        self.guessed_words = []
+        self.letter_colors = []
+        self.actions = 6 * [0] # 6 words
+        self.scores = 6  * [5 * [0]] # 6 words * 5 letters
+        state = np.concatenate([self.actions, *self.scores], axis=0) 
+
+        self.secret_word = random.choice(word_list)
+        return state
+
+    def render(self, return_image=False):
+        labels = []
+        for i,word in enumerate(self.guessed_words):
+            y = (6 - i) * (self.screen_height // 6) - 6
+            for c,(letter,color) in enumerate(zip(word, self.letter_colors[i])):
+                x = (c) * (self.screen_width // 5) + 8
+                text = pyglet.text.Label(
+                    letter, font_name='Monospace',
+                    font_size=10, x=x, y=y,
+                    anchor_x='center', anchor_y='center',
+                    color=color
+                )
+                labels.append(text)
+        
+        self.window.clear()
+        self.window.switch_to()
+        self.window.dispatch_events()
+        for label in labels:
+            label.draw()
+
+        self.window.flip()
+        time.sleep(0.1)
+
+        if return_image:
+            buffer = pyglet.image.get_buffer_manager().get_color_buffer()
+            image_data = buffer.get_image_data()
+            arr = np.frombuffer(image_data.get_data(), dtype=np.uint8)
+            arr = arr.reshape(buffer.height, buffer.width, 4)
+            return arr[..., :3] # leave out alpha
+        
+
+    def close(self):
+        if self.viewer:
+            self.viewer.close()
+            self.viewer = None
